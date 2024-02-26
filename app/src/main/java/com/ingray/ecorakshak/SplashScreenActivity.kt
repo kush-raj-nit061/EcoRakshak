ebde52c52cb0d32cc19b70d06f5595110af11d87
kush-raj-nit061 <kushpvt1@gmail.com>
2024-02-26 19:25:33 +0530

    login and splash screen frontend completed

--- app/src/main/java/com/ingray/ecorakshak/SplashScreenActivity.kt
+++ app/src/main/java/com/ingray/ecorakshak/SplashScreenActivity.kt
@@ -1,11 +1,21 @@
 package com.ingray.ecorakshak
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashScreenActivity : AppCompatActivity() {
+   private lateinit var btn:Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        btn = findViewById(R.id.login)
+        btn.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
